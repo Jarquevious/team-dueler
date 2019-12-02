16648254830def04fec8bd4c2ebbd2f9ec6a2f26
Jarquevious <jarquevious@gmail.com>
2019-12-01 22:11:27 -0800

    methods added/tested

--- superheroes.py
+++ superheroes.py
@@ -60,12 +60,57 @@ class Hero:
             self.starting_health = starting_health
             # when a hero is created, their current health is
             # always the same as their starting health (no damage taken yet!)
-        
+            self.current_health = starting_health    
 
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+        # We used the append method to add strings to a list
+        # in the Rainbow Checklist tutorial. This time,
+        # we're not adding strings, instead we'll add ability objects.
+        self.abilities.append(ability)
+
+    def attack(self):
+        '''Calculate the total damage from all ability attacks.
+          return: total_damage:Int
+        '''
+        # start our total out at 0
+        total_damage = 0
+        # loop through all of our hero's abilities
+        for ability in self.abilities:
+            # add the damage of each attack to our running total
+            total_damage += ability.attack()
+        # return the total damage
+        return total_damage
+
+    def add_armor(self, armor):
+        '''Add armor to self.armors
+        Armor: Armor Object
+        '''
+        # TODO: Add armor object that is passed in to `self.armors`
+        self.armors.append(armor)
+    
+    def defend(self):
+        '''Calculate the total block amount from all armor blocks.
+           return: total_block:Int
+        '''
+        # TODO: This method should run the block method on each armor in self.armors
+        defend_total = 0 
+        for armor in self.armors: 
+             defend_total += armor.block() 
+        return defend_total
+
+    def take_damage(self, damage):
+        '''Updates self.current_health to reflect the damage minus the defense.'''
+        # TODO: Create a method that updates self.current_health to the current
+        # minus the the amount returned from calling self.defend(damage).
+        defense = self.defend()    
+        self.current_health -= damage - defense
+        
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
-
+    hero = Hero("Grace Hopper", 200)
+    shield = Armor("Shield", 50)
+    hero.add_armor(shield)
+    hero.take_damage(50)
+    print(hero.current_health)
